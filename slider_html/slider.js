48ecd926fef227a2c8adf9f2107a2bfa5b729591
Alpscloud <kuptsov@clouds.st>
2016-08-21 17:53:37 +0300

    finished slider which based on OOPgit add .

--- slider_html/slider.js
+++ slider_html/slider.js
@@ -1,86 +1,96 @@
-function Fotorama(elem, preview, len) {
-	// if (!elem) return;
-	this.elem = elem; //контейнер для большого изображения #big-img
-	this.preview = preview; //контейнер маленьких изображений #small-img
-	this.smallImgList = preview.querySelectorAll('img'); //все миниатюры изображения
+function Fotorama(containerImg, sliderList, len) {
+	if (!sliderList) {return;}
+	//контейнер для большого изображения .img-slide
+	this.containerImg = containerImg; 
+	//контейнер маленьких изображений .slide-list
+	this.sliderList = sliderList; 
+	//все миниатюры изображения
+	this.smallImgList = sliderList.querySelectorAll('img');
 	this.outer = document.querySelector('.outer'); 
-	this.active = 'active'; //класс active
-	this.viewport = 'viewport'; // класс viewport 
-	this.btnNext = document.getElementById('btnNext'); //кнопка next #nextBtn
-	this.width = parseInt(window.getComputedStyle(preview.firstElementChild).getPropertyValue('width')); //вычисление ширины одной миниатюры
-	this.count = preview.children.length; //вычисление общего кол-ва миниатюр
-	this.len = len; //сколько выводить миниатюр
+	//класс active
+	this.activeClass = 'active';
+	// класс viewport  
+	this.viewportClass = 'viewport'; 
+	//кнопка next #nextBtn
+	this.btnNext = document.querySelector('.next');
+	//вычисление ширины одной миниатюры 
+	this.width = parseInt(window.getComputedStyle(sliderList.firstElementChild).getPropertyValue('width')); 
+	//вычисление общего кол-ва миниатюр
+	this.count = sliderList.children.length; 
+	//сколько выводить миниатюр
+	this.len = len; 
 
-
-	this.firstElementInit(this.preview, this.elem, this.active);
-	this.cssGenerator(this.preview, 'width', (this.width + 10) * this.count);
+	this.firstElementInit(this.sliderList, this.containerImg, this.activeClass);
+	this.cssGenerator(this.sliderList, 'width', (this.width + 10) * this.count);
 	this.cssGenerator(this.outer, 'width', (this.width + 10) * this.len);
-	this.addListener(this.preview.draw, this.btnNext.handlerToClick);
+	this.addViewport(this.sliderList, this.len, this.viewportClass);
+	this.addListener(this.sliderList, this.btnNext);
 };
-
-// Функция загрузки первого большого изображение в зависимости от первого изображения миниатюры
-Fotorama.prototype.firstElementInit = function () {
-	var first = this.preview.firstElementChild,
-		src = first.src ? first.src : '';
-
-	this.elem.insertAdjacentHTML('afterBegin', '<img src="' + src + '">');
-	first.classList.add(this.active);
-}
 
 // Функция создания свойства для элемента
 Fotorama.prototype.cssGenerator = function (elem, props, value) {
-	elem.style.cssText += props + ':' + value + 'px';
+	elem.style.cssText += props + ':' + (value) + 'px';
 }
 
-// Функция удаление класса active 
-Fotorama.prototype.removeClass = function () {
-	for (var i = 0; i < this.smallImgList.length; i++) {
-		this.smallImgList[i].classList.remove(this.active);
-		this.smallImgList[i].removeAttribute('class');
-	};
-}
 
 // Функция добавление класса viewport миниатюре картинки
 Fotorama.prototype.addViewport = function () {
 	for (var i = 0; i < this.count; i++) {
-		this.preview.children[i].classList.add(this.viewport);
+		if (i < this.len) {this.sliderList.children[i].classList.add(this.viewportClass);}
 	}
 }
 
 // Общая функция добавления событий по клику
 Fotorama.prototype.addListener = function () {
-	this.preview.addEventListener('click', this.draw.bind(this));
+	this.sliderList.addEventListener('click', this.handler.bind(this));
 	this.btnNext.addEventListener('click', this.handlerToClick.bind(this));
 }
 
+// Функция загрузки первого большого изображение в зависимости от первого изображения миниатюры
+Fotorama.prototype.firstElementInit = function () {
+	var first = this.sliderList.firstElementChild,
+		src = first.src ? first.src : '';
+
+	this.containerImg.insertAdjacentHTML('afterBegin', '<img src="' + src + '">');
+	first.classList.add(this.activeClass);
+}
+
+// Функция клика по кнопке NEXT
+Fotorama.prototype.handlerToClick = function () {
+	var active = document.querySelector('.' + this.activeClass);
+	if (active.nextElementSibling && active.nextElementSibling.classList.contains(this.viewportClass)) {
+		this.handler(null, active.nextElementSibling);
+	} else {
+		active.nextElementSibling.classList.add(this.viewportClass);
+		this.cssGenerator(this.sliderList, 'left', (-(this.width + 10)));
+		this.sliderList.firstElementChild.classList.remove(this.viewportClass);
+		this.sliderList.appendChild(this.sliderList.firstElementChild);
+		this.handler(null, active.nextElementSibling);
+		this.cssGenerator(this.sliderList, 'left', (0));
+	}
+}
+
+
 // Функция выбора миниатюры изображения => Большое изображение
-Fotorama.prototype.draw = function(event, el) {
-	var target = event && event.target ? event.target : el,
+Fotorama.prototype.handler = function(event, elem) {
+	var target = event && event.target ? event.target : elem,
           src = target.src ? target.src : "";
 
 	this.removeClass();
 
 	if(target.tagName === 'IMG') {
-		this.elem.firstElementChild.src = src;
-		target.classList.add(this.active);
+		this.containerImg.firstElementChild.src = src;
+		target.classList.add(this.activeClass);
 	} 
 }
 
-// Функция клика по кнопке NEXT
-Fotorama.prototype.handlerToClick = function () {
-	var active = document.querySelector('.' + this.active);
-	if (active.nextElementSibling && active.nextElementSibling.classList.contains(this.viewport)) {
-		this.draw(null, active.nextElementSibling);
-	} else {
-		active.nextElementSibling.classList.add(this.viewport);
-		this.cssGenerator(this.preview, 'left', -(this.width + 10));
-		this.preview.firstElementChild.classList.remove(this.viewport);
-		this.preview.appendChild(this.preview.firstElementChild);
-		this.draw(null, active.nextElementSibling);
-		this.cssGenerator(this.preview, 'left', 0);
+// Функция удаление класса active 
+Fotorama.prototype.removeClass = function () {
+	var active = document.querySelector('.' + this.activeClass);
+	if (active.tagName === 'IMG') {
+		active.classList.remove(this.activeClass);
 	}
 }
 
-new Fotorama(document.querySelector('#big-img'), document.querySelector('#small-img'), 5);
-
+new Fotorama(document.querySelector('.img-slide'), document.querySelector('.slider-list'), 5);
 
